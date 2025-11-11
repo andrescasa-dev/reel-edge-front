78be3e33e7a2c2d9835ed3c5eb596c442051285b
andrescasa-dev <andres.about@gmail.com>
2025-11-11 03:03:09 -0500

    fix: home text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import { Button } from "@/core/components/ui/button";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center gap-4">
-      <p className="text-foreground">Made with passion by andres 🚀</p>
+      <p className="text-foreground">Made with passion by Andr&eacute;s 🚀</p>
       <Link href="/dashboard">
         <Button size="lg">Go to Dashboard</Button>
       </Link>
